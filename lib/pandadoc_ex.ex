a65462959075f5591443c57fcd8b127751a9607a
Nazim Hadj Sadok <nazim@ublo.immo>
2026-02-11 14:27:18 +0100

    feat(documents): implement create document service function

--- lib/pandadoc_ex.ex
+++ lib/pandadoc_ex.ex
@@ -15,4 +15,19 @@ defmodule PandadocEx do
   def hello do
     :world
   end
+
+  def create_document(body_params) do
+    api_key = Application.get_env(:pandadoc_ex, :api_key)
+
+    client = PandadocEx.Client.get_client(api_key)
+
+    url = Application.get_env(:pandadoc_ex, :api_url)
+
+    with Tesla.post(client, "#{url}/documents", body_params) do
+      {:ok, %Tesla.Env{status: 200, body: body}} ->
+        {:ok, body}
+      {:ok, %Tesla.Env{status: status, body: body}} ->
+        {:error, body}
+    end
+  end
 end
---
+++ lib/pandadoc_ex/document.ex
@@ -0,0 +1,18 @@
+defmodule PandadocEx.Document do
+  @moduledoc """
+  Documentation for `PandadocEx.Document`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PandadocEx.Document.hello()
+      :world
+
+  """
+  def create_document(params) do
+
+  end
+end
