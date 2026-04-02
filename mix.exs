23b2534073ac0d9bfcea829976ca2f808a4f6722
Gaetan LUSSON <gaetan@ublo.immo>
2026-04-02 10:43:25 +0200

    chore: bump version to 0.1.1 in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PandadocEx.MixProject do
   def project do
     [
       app: :pandadoc_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
